9139a57102ecc0994c8940bbdfe72057dbaa7420
kyuc2k <wibugi04@gmail.com>
2026-04-16 06:47:03 +0700

    add meta

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if (isset($_SESSION['user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="YAoDQkaWb60XBqE7uJfeSOyeGp5_I_TFJQS46jNx38c" />
     <title>PDF Manager - Hệ thống quản lý file PDF</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
