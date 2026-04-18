952d7bcdf0c205f0d5cd3c5b7de47b8824946bbb
kyuc2k <wibugi04@gmail.com>
2026-04-18 18:07:26 +0700

    rm 404

--- nso.sql
+++ nso.sql
@@ -10,3 +10,13 @@ port INT,
 display_id INT,
 status VARCHAR(20) DEFAULT 'stopped'
 );
+
+
+CREATE TABLE `instances` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `container_name` varchar(100) DEFAULT NULL,
+  `port` int DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
