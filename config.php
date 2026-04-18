fe280c572c847304741e9f16e330e07532d7b963
kyuc2k <wibugi04@gmail.com>
2026-04-18 18:09:11 +0700

    config

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 session_start();
 
 $conn = new mysqli(
-"",
-"",
-"",
-""
+    getenv('DB_HOST') ?: '',
+    getenv('DB_USER') ?: '',
+    getenv('DB_PASSWORD') ?: '',
+    getenv('DB_NAME_NSO') ?: ''
 );
